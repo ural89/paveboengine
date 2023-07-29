ad83540bf5538cfca59b3240d259e79dfe880870
ural89 <uralkemal@gmail.com>
2023-07-29 14:10:30 +0300

    premake lua ready

--- premake5.lua
+++ premake5.lua
@@ -54,3 +54,50 @@ project "PaveboEngine"
 	filter "configurations:Dist"
 		defines "PV_DIST"
 		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	include
+	{
+		"%{prj.name}/vendor/spdlog/include"
+		"PaveboEngine/PaveboEngine/src"
+	}
+	links
+	{
+		"PaveboEngine"
+	}
+	filter "sytem:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"PAVEBO_PLATFORM_WINDOWS"
+		}
+	
+		
+
+	filter "configurations:Debug"
+		defines "PV_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PV_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PV_DIST"
+		optimize "On"
