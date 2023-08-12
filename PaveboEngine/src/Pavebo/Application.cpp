2248df25ff415581081e96ea824aedd9f8a85c61
ural89 <uralkemal@gmail.com>
2023-08-12 19:54:38 +0300

    create window

--- PaveboEngine/src/Pavebo/Application.cpp
+++ PaveboEngine/src/Pavebo/Application.cpp
@@ -3,11 +3,17 @@
 
 
 #include "Event/ApplicatoinEvent.h"
-#include "Event/KeyEvent.h"
+#include "Platform/Windows/WindowsWindow.h"
 namespace Pavebo
 {
 	Application::Application()
 	{
+		WindowProps winProps = WindowProps();
+		winProps.Width = 1200;
+		winProps.Height = 720;
+		winProps.Title = "Game";
+		
+		m_Window = std::unique_ptr<Window>(Window::Create(winProps));
 	}
 
 	Application::~Application()
--- PaveboEngine/src/Pavebo/Application.h
+++ PaveboEngine/src/Pavebo/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "Event/Event.h"
+#include "Window.h"
 namespace Pavebo
 {
 	class PAVEBO_API Application
@@ -9,6 +10,9 @@ namespace Pavebo
 		Application();
 		virtual ~Application();
 		virtual void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		
 
 	};
 	Application* CreateApplication();
