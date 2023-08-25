5c36f1044edc8c9679589a35c907450dcef39a2e
ural89 <uralkemal@gmail.com>
2023-08-25 08:57:33 +0300

    layerstack

---
+++ PaveboEngine/src/Pavebo/Layer.cpp
@@ -0,0 +1,16 @@
+#include "pvpch.h"
+#include "Layer.h"
+
+namespace Pavebo
+{
+	Layer::Layer(const std::string& debugName) :m_DebugName(debugName)
+	{
+		
+
+	}
+
+	Layer::~Layer()
+	{
+
+	}
+}
--- PaveboEngine/src/Pavebo/Layer.h
+++ PaveboEngine/src/Pavebo/Layer.h
@@ -6,7 +6,9 @@ namespace Pavebo {
 
 	class PAVEBO_API Layer
 	{
+	public:
 		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
 		virtual void OnAttach(){}
 		virtual void OnDetach(){}
 		virtual void OnUpdate(){}
